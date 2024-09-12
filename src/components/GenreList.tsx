615142289eb8f16c1a3e56b861e922234459b19c
Rajshekhar Roy <raj.codehub@gmail.com>
2024-09-12 10:22:51 +0530

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,29 @@
-import useGenre from "../hooks/useGenre"
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import useGenre from "../hooks/useGenre";
+import getCroppedImageUrl from "../services/image-url";
+
 
 const GenreList = () => {
-    const {data, error, isLoading} = useGenre()
-    console.log(data)
-return (
-<ul>
-    {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-</ul>
-)
-}
+  const { data, error, isLoading } = useGenre();
+  console.log(data);
+  return (
+    <List>
+      {/* {data.map(genre => <ListItem key={genre.id}><Image src={getCroppedImageUrl(genre.image_background)} boxSize={10} borderRadius="50%"/>{genre.name}</ListItem>)} */}
 
-export default GenreList
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              boxSize={10}
+              borderRadius={8}
+            />
+            <Text fontSize= "lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GenreList;
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,12 +1,11 @@
-
 import useData from "./useData";
 
 export interface Genre {
-    id: number;
-    name: string
+  id: number;
+  name: string;
+  image_background: string;
 }
 
+const useGenre = () => useData<Genre>("/genres");
 
-const useGenre = () => useData<Genre>('/genres')
-
-export default useGenre
+export default useGenre;
