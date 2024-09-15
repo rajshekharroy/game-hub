575bbb93c2a2866acf0b2f91911af5828e8481ed
Rajshekhar Roy <raj.codehub@gmail.com>
2024-09-15 09:28:21 +0530

    Ship genres with the app

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,7 +6,6 @@ import {
   List,
   ListItem,
   Spinner,
-  Text,
 } from "@chakra-ui/react";
 import useGenre, { Genre } from "../hooks/useGenre";
 import getCroppedImageUrl from "../services/image-url";
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,6 @@
-import useData from "./useData";
+// import useData from "./useData";
+
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
@@ -6,6 +8,7 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenre = () => useData<Genre>("/genres");
+// const useGenre = () => useData<Genre>("/genres");  //this will fetch data
+const useGenre = () => ({ data: genres, isLoading: false, error: null }); // but this will take data from data/genres.ts and it will be faster but not updated from the api as genre not frequently change
 
 export default useGenre;
