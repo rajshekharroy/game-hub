9b0a93850da3f2cc433f420b6f530f4d30f1a613
Rajshekhar Roy <raj.codehub@gmail.com>
2024-09-15 07:33:21 +0530

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,12 +16,16 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (
-  gameQuery: GameQuery
-) =>
+const useGames = (gameQuery: GameQuery) =>
   useData<Game>(
     "/games",
-    { params: { genres: gameQuery.genre?.id, platform: gameQuery.platform?.id } },
+    {
+      params: {
+        genres: gameQuery.genre?.id,
+        platform: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+      },
+    },
     [gameQuery]
   );
 
