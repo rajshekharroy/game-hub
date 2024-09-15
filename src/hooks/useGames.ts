4e703bd4d68848201f2e8577bf0fda14057b4713
Rajshekhar Roy <raj.codehub@gmail.com>
2024-09-15 09:16:08 +0530

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -10,10 +10,10 @@ export interface Platform {
 export interface Game {
   id: number;
   name: string;
-  // rating: number;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
