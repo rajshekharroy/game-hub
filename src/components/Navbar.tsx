434fe1b90ea71f1cc61cbc7f99b5e6b86dead6f7
Rajshekhar Roy <raj.codehub@gmail.com>
2024-09-15 09:51:49 +0530

    Customize the theme to get darker gray

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
 interface Props {
   onSearch: (searchText: string) => void;
@@ -12,7 +13,11 @@ const Navbar = ({ onSearch }: Props) => {
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
       <SearchInput onSearch={onSearch} />
-      <ColorModeSwitch />
+      <HStack>
+        <MdLightMode fontSize="20px" />
+        <ColorModeSwitch />
+        <MdDarkMode fontSize="20px" />
+      </HStack>
     </HStack>
   );
 };
