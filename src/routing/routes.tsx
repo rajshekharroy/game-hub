3b013caef17d60b9a2aaf6d020d30c11fb9a9b41
Rajshekhar Roy <raj.codehub@gmail.com>
2024-09-25 08:05:36 +0530

    Final commit

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "../pages/Homepage";
+import HomePage from "../pages/HomePage";
 import Layout from "../pages/Layout";
 import GameDetailPage from "../pages/GameDetailPage";
 import ErrorPage from "../pages/ErrorPage";
